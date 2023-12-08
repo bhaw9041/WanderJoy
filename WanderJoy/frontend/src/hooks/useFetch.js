5af11947b499d2b7dab184473fd81761e23305eb
Himanshu Mal <himanshumaal2000@gmail.com>
2023-12-07 20:17:34 -0500

    Updated hooks

--- WanderJoy/frontend/src/hooks/useFetch.js
+++ WanderJoy/frontend/src/hooks/useFetch.js
@@ -1,26 +1,26 @@
-import {useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 
 const useFetch = (url) => {
-    const [data, setData] = useState([]);
-    const [error, setError] = useState(null);
-    const [loading, setLoading] = useState(false);
+    // const [data, setData] = useState([]);
+    // const [error, setError] = useState(null);
+    // const [loading, setLoading] = useState(false);
 
     useEffect(() => {
-        const fetchData = async() => {
+        const fetchData = async () => {
             setLoading(true);
 
-            try{
+            try {
                 const res = await fetch(url)
 
-                if(!res.ok){
+                if (!res.ok) {
                     setError('failed to fetch');
-                    
+
                 }
                 const result = await res.json();
                 setData(result.data);
                 setLoading(false);
 
-            } catch(err) {
+            } catch (err) {
                 setError(err.message);
                 setLoading(false);
             }
